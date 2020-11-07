73aa979d15be9ed1582c6c54e24f5fdd72c621c7
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-11-07 16:36:30 +0700

    initial commit

--- src/com/userlogin/Latihan33Login.java
+++ src/com/userlogin/Latihan33Login.java
@@ -2,6 +2,16 @@ package com.userlogin;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author User
+ * NAMA         : Ivan Faathirza
+ * KELAS        : IF1
+ * NIM          : 10119003
+ * Deskripsi Program : ini adalah program login untuk mengecek username dan password
+ * berdasarkan instansiasi objek dari user
+ */
+
 public class Latihan33Login {
     private static String usName;
     private static String passWord;
